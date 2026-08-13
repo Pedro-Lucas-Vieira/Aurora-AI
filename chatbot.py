88584574a06600000ca279f1b079e7c74246e451
Pedro lucas <ps809624@gmail.com>
2026-08-13 16:58:47 -0300

    Ajusta modelo para gemini-1.5-flash-002

--- chatbot.py
+++ chatbot.py
@@ -57,7 +57,7 @@ print("Base vetorial carregada com sucesso.")
 
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-flash",  
+    model="gemini-1.5-flash-002",  
     google_api_key=GOOGLE_API_KEY,
     temperature=0.2
 
